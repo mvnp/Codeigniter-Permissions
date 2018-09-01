040997a44b07e021c12bfdb15fe93d11be53f16f
Marcos Vinicius Nasc. Pereira <mvnpereira@gmail.com>
2018-09-01 14:38:09 -0300

    Sistema de Permissões baseado em Métodos para Codeigniter 3

--- database/prmissions.sql
+++ database/prmissions.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
+-- version 4.4.15.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: 01-Set-2018 às 15:34
--- Versão do servidor: 5.7.19
--- PHP Version: 5.6.37
+-- Host: mysql472.umbler.com
+-- Generation Time: 01-Set-2018 às 14:35
+-- Versão do servidor: 5.6.40-log
+-- PHP Version: 5.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prmissions`
+-- Database: `tasks`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +26,7 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `rotas`
 --
 
-CREATE TABLE `rotas` (
+CREATE TABLE IF NOT EXISTS `rotas` (
   `ID` int(11) NOT NULL,
   `CLASS` varchar(256) COLLATE utf8mb4_swedish_ci NOT NULL,
   `METHOD` varchar(256) COLLATE utf8mb4_swedish_ci NOT NULL,
@@ -37,23 +35,23 @@ CREATE TABLE `rotas` (
   `STATUS` int(1) NOT NULL,
   `DESCRIPTION` text COLLATE utf8mb4_swedish_ci NOT NULL,
   `DTCAD` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 --
 -- Extraindo dados da tabela `rotas`
 --
 
 INSERT INTO `rotas` (`ID`, `CLASS`, `METHOD`, `CLASSMETHOD`, `VISIBILITY`, `STATUS`, `DESCRIPTION`, `DTCAD`) VALUES
-(24, 'clientes', 'index', 'clientes/index', 1, 1, 'Aguardando descrição', '2018-09-01 11:26:55'),
-(25, 'clientes', 'listar', 'clientes/listar', 1, 1, 'Aguardando descrição', '2018-09-01 11:26:55'),
-(26, 'welcome', 'index', 'welcome/index', 1, 1, 'Aguardando descrição', '2018-09-01 11:26:55'),
-(27, 'clientes', 'escolher', 'clientes/escolher', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
-(28, 'clientes', 'comprar', 'clientes/comprar', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
-(29, 'clientes', 'pagarparcelas', 'clientes/pagarparcelas', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
-(30, 'clientes', 'fazerparcelamento', 'clientes/fazerparcelamento', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
-(31, 'clientes', 'fiado', 'clientes/fiado', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
-(32, 'clientes', 'devolver', 'clientes/devolver', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
-(33, 'clientes', 'trocar', 'clientes/trocar', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:31');
+(1, 'clientes', 'index', 'clientes/index', 1, 1, 'Aguardando descrição', '2018-09-01 11:26:55'),
+(2, 'clientes', 'listar', 'clientes/listar', 1, 1, 'Aguardando descrição', '2018-09-01 11:26:55'),
+(3, 'welcome', 'index', 'welcome/index', 1, 1, 'Aguardando descrição', '2018-09-01 11:26:55'),
+(4, 'clientes', 'escolher', 'clientes/escolher', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
+(5, 'clientes', 'comprar', 'clientes/comprar', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
+(6, 'clientes', 'pagarparcelas', 'clientes/pagarparcelas', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
+(7, 'clientes', 'fazerparcelamento', 'clientes/fazerparcelamento', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
+(8, 'clientes', 'fiado', 'clientes/fiado', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
+(9, 'clientes', 'devolver', 'clientes/devolver', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:30'),
+(10, 'clientes', 'trocar', 'clientes/trocar', 1, 1, 'Aguardando descrição', '2018-09-01 11:36:31');
 
 -- --------------------------------------------------------
 
@@ -61,13 +59,13 @@ INSERT INTO `rotas` (`ID`, `CLASS`, `METHOD`, `CLASSMETHOD`, `VISIBILITY`, `STAT
 -- Estrutura da tabela `rotas_grupos`
 --
 
-CREATE TABLE `rotas_grupos` (
+CREATE TABLE IF NOT EXISTS `rotas_grupos` (
   `R_ID` int(11) NOT NULL,
   `R_NAME` varchar(256) COLLATE utf8mb4_swedish_ci NOT NULL,
   `R_DESCRIPTION` text COLLATE utf8mb4_swedish_ci NOT NULL,
   `R_STATUS` int(1) NOT NULL,
   `R_DTCAD` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 --
 -- Extraindo dados da tabela `rotas_grupos`
@@ -84,13 +82,13 @@ INSERT INTO `rotas_grupos` (`R_ID`, `R_NAME`, `R_DESCRIPTION`, `R_STATUS`, `R_DT
 -- Estrutura da tabela `rotas_grupos_metodos`
 --
 
-CREATE TABLE `rotas_grupos_metodos` (
+CREATE TABLE IF NOT EXISTS `rotas_grupos_metodos` (
   `RG_ID` int(11) NOT NULL,
   `RG_GROUP` int(11) NOT NULL,
   `RG_METHOD` int(11) NOT NULL,
   `RG_STATUS` int(11) NOT NULL,
   `RG_DTCAD` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 --
 -- Extraindo dados da tabela `rotas_grupos_metodos`
@@ -116,12 +114,12 @@ INSERT INTO `rotas_grupos_metodos` (`RG_ID`, `RG_GROUP`, `RG_METHOD`, `RG_STATUS
 -- Estrutura da tabela `rotas_grupos_usuarios`
 --
 
-CREATE TABLE `rotas_grupos_usuarios` (
+CREATE TABLE IF NOT EXISTS `rotas_grupos_usuarios` (
   `RU_ID` int(11) NOT NULL,
   `RU_USUARIO` int(11) NOT NULL,
   `RU_GRUPO` int(11) NOT NULL,
   `RU_DTCAD` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 --
 -- Extraindo dados da tabela `rotas_grupos_usuarios`
@@ -142,26 +140,25 @@ INSERT INTO `rotas_grupos_usuarios` (`RU_ID`, `RU_USUARIO`, `RU_GRUPO`, `RU_DTCA
 -- Indexes for table `rotas`
 --
 ALTER TABLE `rotas`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `CLASSMETHOD` (`CLASSMETHOD`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `rotas_grupos`
 --
 ALTER TABLE `rotas_grupos`
-  ADD PRIMARY KEY (`R_ID`);
+  ADD PRIMARY KEY (`R_ID`) USING BTREE;
 
 --
 -- Indexes for table `rotas_grupos_metodos`
 --
 ALTER TABLE `rotas_grupos_metodos`
-  ADD PRIMARY KEY (`RG_ID`);
+  ADD PRIMARY KEY (`RG_ID`) USING BTREE;
 
 --
 -- Indexes for table `rotas_grupos_usuarios`
 --
 ALTER TABLE `rotas_grupos_usuarios`
-  ADD PRIMARY KEY (`RU_ID`);
+  ADD PRIMARY KEY (`RU_ID`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -171,27 +168,22 @@ ALTER TABLE `rotas_grupos_usuarios`
 -- AUTO_INCREMENT for table `rotas`
 --
 ALTER TABLE `rotas`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
-
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `rotas_grupos`
 --
 ALTER TABLE `rotas_grupos`
-  MODIFY `R_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
+  MODIFY `R_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `rotas_grupos_metodos`
 --
 ALTER TABLE `rotas_grupos_metodos`
-  MODIFY `RG_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+  MODIFY `RG_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `rotas_grupos_usuarios`
 --
 ALTER TABLE `rotas_grupos_usuarios`
-  MODIFY `RU_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
+  MODIFY `RU_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
